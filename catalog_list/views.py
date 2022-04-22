bbcac8e4f9be991a442e903280e6e7d0ddce43ec
devShev <aalshe38@gmail.com>
2022-04-22 14:01:35 +0300

    views and templates

--- catalog_list/views.py
+++ catalog_list/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, FormView
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, FormView, TemplateView
 
 from .models import Ad, ItemType
-from utils import DataMixin
+from .utils import DataMixin
 
 
 class CatalogHome(DataMixin, ListView):
@@ -12,7 +12,32 @@ class CatalogHome(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Каталог')
+        types = ItemType.objects.all()
+        context['types'] = types
+        c_def = self.get_user_context(title='Каталог', cuurent_menu='home')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Ad.objects.all().select_related('author', 'type')
+
+
+def create(request):
+    return HttpResponse("Создание записи")
+
+
+class Contact(DataMixin, TemplateView):
+    template_name = 'catalog_list/contact.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Связь с нами', cuurent_menu='contact')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class Rules(DataMixin, TemplateView):
+    template_name = 'catalog_list/rules.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Правила площадки', cuurent_menu='rules')
+        return dict(list(context.items()) + list(c_def.items()))
